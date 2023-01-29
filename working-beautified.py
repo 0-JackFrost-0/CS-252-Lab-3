7bb2643132818058158fd49d06ad4854dea851b7
CelestialBlade1 <vijaybalsubramaniam1@gmail.com>
2023-01-29 20:36:08 +0530

    beautified prototype

--- working-beautified.py
+++ working-beautified.py
@@ -6,12 +6,12 @@ import numpy as np
 def bits_to_notes(bits):
     note_map = {
         "000": "Sa",
-        "001": "Ri1",
-        "010": "Ga2",
+        "001": "Re1",
+        "010": "Ga3",
         "011": "Ma1",
         "100": "Pa",
         "101": "Dha1",
-        "110": "Ni2",
+        "110": "Ni3",
         "111": "SaU",
     }
     return [note_map[bits[i : i + 3]] for i in range(0, len(bits), 3)]
@@ -21,17 +21,17 @@ def bits_to_notes(bits):
 sa_frequency = 280  # Sa frequency in Hz
 carnatic_notes = {
     "Sa": sa_frequency,
-    "Ri1": sa_frequency * 256 / 243,
-    "Ri2": sa_frequency * 9 / 8,
-    "Ga1": sa_frequency * 32 / 27,
-    "Ga2": sa_frequency * 81 / 64,
+    "Re1": sa_frequency * 256 / 243,
+    "Re2": sa_frequency * 9 / 8,
+    "Ga2": sa_frequency * 32 / 27,
+    "Ga3": sa_frequency * 81 / 64,
     "Ma1": sa_frequency * 4 / 3,
     "Ma2": sa_frequency * 729 / 512,
     "Pa": sa_frequency * 3 / 2,
     "Dha1": sa_frequency * 128 / 81,
     "Dha2": sa_frequency * 27 / 16,
-    "Ni1": sa_frequency * 16 / 9,
-    "Ni2": sa_frequency * 243 / 128,
+    "Ni2": sa_frequency * 16 / 9,
+    "Ni3": sa_frequency * 243 / 128,
     "SaU": sa_frequency * 2,
 }
 
@@ -90,15 +90,15 @@ while True:
             ):
                 print("Ma1")
                 received_data.append("011")
-            elif (frequency > carnatic_notes["Ri1"] - 5) and (
-                carnatic_notes["Ri1"] + 5 > frequency
+            elif (frequency > carnatic_notes["Re1"] - 5) and (
+                carnatic_notes["Re1"] + 5 > frequency
             ):
                 print("Re1")
                 received_data.append("001")
-            elif (frequency > carnatic_notes["Ga2"] - 5) and (
-                carnatic_notes["Ga2"] + 5 > frequency
+            elif (frequency > carnatic_notes["Ga3"] - 5) and (
+                carnatic_notes["Ga3"] + 5 > frequency
             ):
-                print("Ga2")
+                print("Ga3")
                 received_data.append("010")
             elif (frequency > carnatic_notes["Pa"] - 5) and (
                 carnatic_notes["Pa"] + 5 > frequency
@@ -110,20 +110,21 @@ while True:
             ):
                 print("Dha1")
                 received_data.append("101")
-            elif (frequency > carnatic_notes["Ni2"] - 5) and (
-                carnatic_notes["Ni2"] + 5 > frequency
+            elif (frequency > carnatic_notes["Ni3"] - 5) and (
+                carnatic_notes["Ni3"] + 5 > frequency
             ):
-                print("Ni2")
+                print("Ni3")
                 received_data.append("110")
             elif (frequency > carnatic_notes["SaU"] - 5) and (
                 carnatic_notes["SaU"] + 5 > frequency
             ):
-                print("Sa'")
+                print("SaU")
                 received_data.append("111")
             else:
                 print("Nothing")
 
             rec_freq.append(frequency)
+
 
             Done = True
             if len(rec_freq) < 20:
@@ -131,10 +132,13 @@ while True:
 
             else:
                 for a in rec_freq:
-                    if a > 150:
+                    if a > 200:
                         Done = False
                 rec_freq.pop(0)
-
-        print(received_data)
+        
+        rec_data = ''
+        for strn in received_data:
+            rec_data = rec_data + strn 
+        print("Message received is: {rec_data}")
     else:
         break
